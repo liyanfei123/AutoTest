1b340127d81a32cb13f1676000cf35a6c7b8dd50
liyanfei01 <liyanfei01@corp.netease.com>
2022-11-07 16:20:55 +0800

    error

--- src/main/java/com/testframe/autotest/core/exception/AutoTestException.java
+++ src/main/java/com/testframe/autotest/core/exception/AutoTestException.java
@@ -2,10 +2,17 @@ package com.testframe.autotest.core.exception;
 
 public class AutoTestException extends RuntimeException {
 
+    private Integer errCode;
+
     public AutoTestException() {}
 
     public AutoTestException(String msg) {
         super(msg);
+    }
+
+    public AutoTestException(Integer errorCode, String msg) {
+        super(msg);
+        this.errCode = errorCode;
     }
 
 }
